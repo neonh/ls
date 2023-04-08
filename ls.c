a025dcaca26ef9c06faba3a6da41312b4b1a5183
Igor <savochkin@physics.msu.ru>
2023-04-08 21:40:10 +0300

    Upgrade for multiple directories

--- ls.c
+++ ls.c
@@ -6,10 +6,12 @@
 #include <stdlib.h>
 #include <dirent.h>
 
-#define MIN_ARG_QTY     (2)
+#define MIN_ARG_QTY                 (2)
+#define FIRST_DIR_ARG_NUM           (MIN_ARG_QTY)
 
 
 static void ls_l(const char* const dir);
+
 
 int main(int argc, const char *argv[])
 {
@@ -21,21 +23,29 @@ int main(int argc, const char *argv[])
         (argv[1][1] == 'l') &&
         (argv[1][2] == 0U))
 	{
-        int dir_qty = argc - MIN_ARG_QTY;
+        int dir_qty = argc - FIRST_DIR_ARG_NUM;
+        // Default directory
+        const char* dirname = ".";
 
-        if (dir_qty == 0)
+        if (dir_qty > 0)
         {
-            ls_l(".");
-        }
-        else
-        {
-            for (int i = 0; i < dir_qty; i++)
+            // If directory name specified
+            dirname = argv[FIRST_DIR_ARG_NUM];
+            if (dir_qty > 1)
             {
-                const char* dirname = argv[MIN_ARG_QTY + i];
+                // If more than one directory specified - print dirname before output
                 printf("%s:\n", dirname);
-                ls_l(dirname);
-                printf("\n");
             }
+        }
+        // Output for first directory
+        ls_l(dirname);
+
+        // For other directories
+        for (int i = 1; i < dir_qty; i++)
+        {
+            dirname = argv[FIRST_DIR_ARG_NUM + i];
+            printf("\n%s:\n", dirname);
+            ls_l(dirname);
         }
 
         ret = EXIT_SUCCESS;
@@ -52,10 +62,11 @@ int main(int argc, const char *argv[])
 static void ls_l(const char* const dirname)
 {
 	struct dirent *d;
-	DIR *dirs = opendir(dirname);
-	if (dirs != NULL)
+	DIR *dir = opendir(dirname);
+
+	if (dir != NULL)
 	{
-        while ((d = readdir(dirs)) != NULL)
+        while ((d = readdir(dir)) != NULL)
         {
             // Ignore hidden
             if (d->d_name[0] != '.')
@@ -63,5 +74,6 @@ static void ls_l(const char* const dirname)
                 printf("%s\n", d->d_name);
             }
         }
+        closedir(dir);
     }
 }
