ac66a09d42c396ab05cda03624eaeb7ae364acea
Igor <savochkin@physics.msu.ru>
2023-04-11 14:02:35 +0300

    Fixed fields length

--- ls.c
+++ ls.c
@@ -40,7 +40,7 @@ const char PERMISSION[PERMISSION_QTY] = "rwx";
 
 // Number for initial memory allocation
 #define INIT_FILES_QTY              (64U)
-// Maximum allowed lengthes for filenames and usernames
+// Maximum allowed lengths for filenames and usernames
 #define FNAME_LEN_MAX               (255U)
 #define USERNAME_LEN_MAX            (32U)
 
@@ -60,7 +60,7 @@ typedef struct f_info_st
 
 static void mode_to_str(const mode_t mode, char* const str);
 static int cmp_fn(const void* f1, const void* f2);
-static void print_file_info(const f_info_t* const f);
+static void print_files_info(const f_info_t* const files, const size_t qty);
 static int ls_l(const char* const dir);
 
 
@@ -167,35 +167,72 @@ static int cmp_fn(const void* f1, const void* f2)
     return strcmp(((f_info_t*)f1)->fname, ((f_info_t*)f2)->fname);
 }
 
-static void print_file_info(const f_info_t* const f)
+static void print_files_info(const f_info_t* const files, const size_t qty)
 {
-    struct tm* time;
-    char time_str[TIME_STR_LEN + 1U];
-    char mode_str[MODE_STR_LEN + 1U];
+    off_t max_size = 10;
+    size_t max_size_len = 1U;
+    nlink_t max_nlink = 10U;
+    size_t max_nlink_len = 1U;
+    size_t max_user_len = 1U;
+    size_t max_group_len = 1U;
 
-    // Mode
-    mode_to_str(f->mode, mode_str);
-    printf("%s ", mode_str);
+    // Find max lengths of print fields
+    for (size_t i = 0U; i < qty; i++)
+    {
+        const f_info_t* f = &files[i];
+        size_t len;
 
-    // Links quantity
-    printf("%ld ", f->nlink);
+        len = strlen(f->user);
+        if (len > max_user_len) max_user_len = len;
 
-    // User
-    printf("%s ", f->user);
+        len = strlen(f->group);
+        if (len > max_group_len) max_group_len = len;
 
-    // Group
-    printf("%s ", f->group);
+        if (f->nlink >= max_nlink)
+        {
+            max_nlink *= 10U;
+            max_nlink_len++;
+        }
 
-    // Size
-    printf("%ld ", f->size);
+        if (f->size >= max_size)
+        {
+            max_size *= 10U;
+            max_size_len++;
+        }
+    }
 
-    // Modification time
-    time = localtime(&f->mtime);
-    strftime(time_str, sizeof(time_str), "%b %e %H:%M", time);
-    printf("%s ", time_str);
+    // Print
+    for (size_t i = 0U; i < qty; i++)
+    {
+        const f_info_t* f = &files[i];
+        struct tm* time;
+        char time_str[TIME_STR_LEN + 1U];
+        char mode_str[MODE_STR_LEN + 1U];
 
-    // Name
-    printf("%s\n", f->fname);
+        // Mode
+        mode_to_str(f->mode, mode_str);
+        printf("%s ", mode_str);
+
+        // Links quantity
+        printf("%*ld ", (int)max_nlink_len, f->nlink);
+
+        // User
+        printf("%*s ", (int)max_user_len, f->user);
+
+        // Group
+        printf("%*s ", (int)max_group_len, f->group);
+
+        // Size
+        printf("%*ld ", (int)max_size_len, f->size);
+
+        // Modification time
+        time = localtime(&f->mtime);
+        strftime(time_str, sizeof(time_str), "%b %e %H:%M", time);
+        printf("%s ", time_str);
+
+        // Name
+        printf("%s\n", f->fname);
+    }
 }
 
 static int ls_l(const char* const dirname)
@@ -210,10 +247,6 @@ static int ls_l(const char* const dirname)
         f_info_t* files;
         size_t f_cnt = 0U;
         size_t f_alloc_qty = INIT_FILES_QTY;
-
-        // TODO
-        // 1 find max len of size and links fields
-        size_t max_fname_len = 0U;
 
         files = malloc(sizeof(f_info_t) * f_alloc_qty);
 
@@ -246,11 +279,8 @@ static int ls_l(const char* const dirname)
                     strcpy(files[f_cnt].user, user->pw_name);
                     strcpy(files[f_cnt].group, group->gr_name);
                     files[f_cnt].size = st.st_size;
+                    files[f_cnt].mtime = st.st_mtime;
                     strcpy(files[f_cnt].fname, d->d_name);
-
-                    // TODO
-                    len = strlen(d->d_name);
-                    max_fname_len = (len > max_fname_len)? len: max_fname_len;
 
                     // Increase number of files
                     f_cnt++;
@@ -266,10 +296,7 @@ static int ls_l(const char* const dirname)
         }
 
         // Print
-        for (size_t i = 0U; i < f_cnt; i++)
-        {
-            print_file_info(&files[i]);
-        }
+        print_files_info(files, f_cnt);
         free(files);
 
         ret = EXIT_SUCCESS;
